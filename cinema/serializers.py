f758bc5ab44ec959edd2b29659b0aaa69d25807a
Prus Oleksandr <cheepaxxa2003@gmail.com>
2026-06-04 10:05:23 +0300

    Fix  serializer

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -74,6 +74,12 @@ class MovieDetailSerializer(MovieSerializer):
         )
 
 
+class TicketListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ("row", "seat")
+
+
 class MovieSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieSession
@@ -105,14 +111,6 @@ class MovieSessionListSerializer(MovieSessionSerializer):
             "cinema_hall_capacity",
             "tickets_available",
         )
-
-
-class TicketListSerializer(serializers.ModelSerializer):
-    movie_session = MovieSessionListSerializer(read_only=True)
-
-    class Meta:
-        model = Ticket
-        fields = ("row", "seat", "movie_session")
 
 
 class MovieSessionDetailSerializer(MovieSessionSerializer):
